535b0ed6ce64f5d0fb7ac90aa410b9cda340d841
ninacia <n.cialdella@outlook.com>
2017-05-15 18:17:27 +0200

    bf

--- breadthfirst1.py
+++ breadthfirst1.py
@@ -1,93 +1,82 @@
 """
 First breadth first search algorithm to find a mutation sequence that turns one 'genome' into another
+MET ARCHIEF
 
 Leander
 Nina
 
-started: 2017-5-11
+started: 2017-5-15
 """
-from mutations import mutationlist
 
+def main(geneOrigin =  [5,2,7,6,8,1,4,3], maxDepth = 4, printer = True):
+    import sys
+    import os 
+    from mutations import mutationlist
+    from Queue import Queue
+    import time
 
-# geneOrigin = [4,3,2,1]
-geneOrigin = [3,5,1,7,8,2,4,6] # test Genome that has to change to a sorted array
-geneOrigin = [11, 6, 1, 2, 10, 7, 5, 8, 12, 13, 14, 15, 16, 17, 3, 4, 9]
-geneLength = len(geneOrigin)
-genes = []
-genes.append(geneOrigin)
-solution = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+    # geneOrigin = [4,3,2,1]
+    geneLength = len(geneOrigin)
+    genes = Queue()
+    genes.put(geneOrigin)
 
-mutationTrack = [-1] # This keeps track of how many mutations have been tried for a child
+    # Create solution array
+    solution = []
+    for i in range(1,geneLength+1):
+        solution.append(i)
 
-# Create the list of possible mutations in this genome
-mut = mutationlist(geneLength)
+    mutationTrack = [-1] # This keeps track of how many mutations have been tried for a child
+    # Create the list of possible mutations in this genome
+    mut = mutationlist(geneLength)
+    print(len(mut.start))
+    # Breadth First Search
+    archive = dict()
+    archive["".join(str(x) for x in geneOrigin)] = True
+    Go = True
+    doubleCounter = 0
+    tstart = time.time()
+    level = 0
 
-# Depth first search
-archive = dict()
-archive["".join(str(x) for x in geneOrigin)] = True
-Go = True
-maxDepth = 8
-doubleCounter = 0
+    while Go:
 
-while Go:
-    # stap 1: Kinderen maken
-    child = genes[-1][:]
-    # print("\n {}".format(genes))
-    mutation = mutationTrack.pop() + 1
+        # stap 1: Alle mogelijke kinderen maken en opslaan - checken of een van de kinderen de oplossing is. 
+        level += 1
+        mother = genes.get()
 
-    while (mutation >= mut.max):
-        # print("pruned because all mutation of this node have been tried {}".format(mutation))
-        # Go up one level and continue with the other mutation
-        genes.pop()
-        mutation = mutationTrack.pop() + 1
+        #print(mother)
 
-        # print("going to do mutation {}".format(mutation))
+        # mutate the child - add to queu if not already in archive nor solution of the problem 
+        for i in range (0, 27):     
+            child = mother
+            child[mut.start[i]:mut.end[i]] = child[mut.start[i]:mut.end[i]][::-1]
+            #print(child)
+            key = "".join(str(x) for x in child)
 
-    mutationTrack.append(mutation)  # mutation  kept track of
+            # check if child is the solution
+            if child == solution:
+                Go = False
+                print("wiehoe")
+                print(level)
+                genes.put(child)
+                break
+            # check if child is already in the archive - if so don't add this child to the queu
+            elif (archive.get(key, False) != False):
+                doubleCounter += 1
 
-    # print("unaltered child {}\n".format(child))
-    # print("\nmutation {}: section to flip: [{}]:[{}] = {} -> {}".format(mutation, mut.start[mutation], mut.end[mutation],
-    #     child[mut.start[mutation]:mut.end[mutation]],  child[mut.start[mutation]:mut.end[mutation]][::-1]))
+            # child is not the solution nor in archive - so should be added to the end of thequeu and archive
+            else:
+                genes.put(child)
+                archive[key] = True
+                #print("hallo")
 
-    # Stap 2: Mutate the child, according to the latest mutation of this 'node'
-    child[mut.start[mutation]:mut.end[mutation]] = child[mut.start[mutation]:mut.end[mutation]][::-1]
+    tduration = time.time() - tstart
 
-    key = "".join(str(x) for x in child)
+    print('number of genomes in archive: {}'.format(len(archive)))
+    print('# of double found sequences:  {}'.format(doubleCounter))
+    #print("archive:\n")
+    #print(archive)
 
-    if child == solution:
-        # Stap 5: Controleren of een van de nieuwe kinderen de oplossing is
-        Go = False
-        print("\nWOHOOOOOO\n\n")
-        genes.append(child[:])
+    print("program took {0:.3f} seconds to find solution".format(tduration))
 
-    # stap 3: Controleren of het unieke kinnderen zijn
-    elif (archive.get(key, False) != False) and (len(mutationTrack) > archive.get(key,100)): # Child already found
-        # doubleCounter += 1
-        # print("non-unique value encountered for #{} time".format(doubleCounter))
-        print("mutationtracker: {}".format(mutationTrack))
-
-    else:
-        archive[key] = len(mutationTrack)
-
-        if len(genes) > maxDepth:
-            # Stoppen met tak als ie te diep wordt
-            genes.pop()
-            mutationTrack.pop()
-            # print("pruned because of branch depth > {}".format(maxDepth))
-
-        else:
-            # Stap 7: Kinderen toevoegen aan stack, ze gaan ouders worden! Stap1
-            genes.append(child[:])
-            mutationTrack.append(-1)
-
-print('final amount of double found sequences: {}'.format(doubleCounter))
-#print("archive:\n")
-#print(archive)
-print("\ni,   start,  length, ending")
-for i in range(len(mut.start)):
-    print("[{0:<2}]: {1:<7} : {2:<7} : {3:<7}".format(i, mut.start[i]+1, mut.length[i], mut.end[i]))
-print("number of possible mutations = {}\n".format(mut.max))
-
-for gene in genes:
-    print(gene)
-print("final mutation tracker: {}".format(mutationTrack))
+if __name__ == '__main__':
+    main()
