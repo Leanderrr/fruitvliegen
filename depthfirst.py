d50e89ca6035281f3d5ba12997d91bfd50f58b2e
Leanderrr <leanderdk@gmail.com>
2017-05-16 16:46:25 +0200

    Plot function completion

--- depthfirst.py
+++ depthfirst.py
@@ -5,6 +5,10 @@ Leander
 Nina
 """
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib import cm
+from numpy import linspace
+
 
 def new_branch(genes, mutationTrack):
     """
@@ -31,30 +35,39 @@ def plotMutations(genes, mutationTrack, mut):
         mutationTrack: The mutations that were done
         mut: The meaning of mutations (object with 3 fields created in the mutationlist function)
     """
+    cm_subsection = linspace(0, 1, len(genes[0])+2)
+    colors = [cm.summer(x) for x in cm_subsection]
+
+
     fig = plt.figure(figsize=(10, 15))
     plt.title("mutation sequence")
     ax = fig.add_subplot(111)
-    i = 0
+    y = 0
     for genome in genes:
         # Print genome text in figure
-        j = 0
+        x = 0
         for gen in genome:
-            ax.text(j/4, i, '{}, '.format(gen))
-            j += 1
-        i += 1
+            ax.text(x, y, '{}, '.format(gen))
+            # Add colored rectangle showing gen value height
+            ax.add_patch(patches.Rectangle(
+                (x-0.125, y-0.1), 0.25, 0.25,
+                facecolor=(colors[gen][0], colors[gen][1], colors[gen][2],0.7),
+                edgecolor="none"))
+            x += 1/4
+        y += 1
 
     i = 0
     for mutation in mutationTrack:
         # Plot mutation lines
-        x1 = (mut.start[mutation] + 0.25)/4
-        x2 = (mut.end[mutation] - 1 + 0.25)/4
-        y1 = i + 0.3
+        x1 = (mut.start[mutation] + 0.15)/4
+        x2 = (mut.end[mutation] - 1 + 0.15)/4
+        y1 = i + 0.15
         y2 = i + 0.9
         i += 1
-        ax.plot([x1, x2], [y1, y2], color='g')
-        ax.plot([x1, x2], [y2, y1], color='g')
+        ax.plot([x1, x2], [y1, y2], color=colors[mut.length[mutation]+1], linewidth=3)
+        ax.plot([x1, x2], [y2, y1], color=colors[mut.length[mutation]+1], linewidth=3)
 
 
     plt.ylabel("mutation (n)")
-    ax.axis([-0.5, len(genes)/2, -0.5, len(genes)])
+    ax.axis([-0.25, len(genes)/2, -0.5, len(genes)])
     plt.show()
