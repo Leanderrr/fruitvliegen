bb3fb518bd9ea8da3b0a06fe168603a46303b05b
Leanderrr <leanderdk@gmail.com>
2017-05-16 12:06:44 +0200

    Breadthfirst functioneel

--- breadthfirst1.py
+++ breadthfirst1.py
@@ -8,12 +8,11 @@ Nina
 started: 2017-5-15
 """
 
-def main(geneOrigin =  [5,2,7,6,8,1,4,3], maxDepth = 4, printer = True):
-    import sys
-    import os 
-    from mutations import mutationlist
-    from Queue import Queue
-    import time
+from mutations import mutationlist
+from queue import Queue
+import time
+
+def main(geneOrigin =  [5,2,6,1,4,3], maxDepth = 4, printer = True):
 
     # geneOrigin = [4,3,2,1]
     geneLength = len(geneOrigin)
@@ -25,40 +24,47 @@ def main(geneOrigin =  [5,2,7,6,8,1,4,3], maxDepth = 4, printer = True):
     for i in range(1,geneLength+1):
         solution.append(i)
 
-    mutationTrack = [-1] # This keeps track of how many mutations have been tried for a child
     # Create the list of possible mutations in this genome
     mut = mutationlist(geneLength)
-    print(len(mut.start))
+    print("max mutations per mother: {}".format(mut.max))
+
     # Breadth First Search
     archive = dict()
-    archive["".join(str(x) for x in geneOrigin)] = True
+    key = "".join(str(x) for x in geneOrigin)
+    archive[key] = [0] # The value is the depth level of this gene sequence
     Go = True
     doubleCounter = 0
     tstart = time.time()
-    level = 0
 
     while Go:
+        # stap 1: Alle mogelijke kinderen maken en opslaan - checken of een van de kinderen de oplossing is. (dat is een grote stap)
 
-        # stap 1: Alle mogelijke kinderen maken en opslaan - checken of een van de kinderen de oplossing is. 
-        level += 1
         mother = genes.get()
+        motherkey = "".join(str(x) for x in mother)
 
+        print("I'm going to start over in the while loop {}".format(motherkey))
+        level = archive[motherkey][0] + 1
+
+        print(motherkey)
+        print(archive[motherkey])
         #print(mother)
 
-        # mutate the child - add to queu if not already in archive nor solution of the problem 
-        for i in range (0, 27):     
-            child = mother
+        # mutate the child - add to queue if not already in archive nor solution of the problem
+        for i in range(0, mut.max):
+            print('entered for loop: {}'.format(i))
+
+            child = mother[:]
             child[mut.start[i]:mut.end[i]] = child[mut.start[i]:mut.end[i]][::-1]
-            #print(child)
+            print(child)
             key = "".join(str(x) for x in child)
 
             # check if child is the solution
             if child == solution:
                 Go = False
                 print("wiehoe")
-                print(level)
                 genes.put(child)
                 break
+
             # check if child is already in the archive - if so don't add this child to the queu
             elif (archive.get(key, False) != False):
                 doubleCounter += 1
@@ -66,8 +72,9 @@ def main(geneOrigin =  [5,2,7,6,8,1,4,3], maxDepth = 4, printer = True):
             # child is not the solution nor in archive - so should be added to the end of thequeu and archive
             else:
                 genes.put(child)
-                archive[key] = True
+                archive[key] = [level, i]
                 #print("hallo")
+        print("it's really going well at {}".format(motherkey))
 
     tduration = time.time() - tstart
 
