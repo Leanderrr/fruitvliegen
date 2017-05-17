5e9f5bebd77f64d3fb6cec83d1474c78b3b6c1f6
ninacia <n.cialdella@outlook.com>
2017-05-17 20:25:01 +0200

    Costfunction 2 + pp 2 deel

---
+++ costfunction2.py
@@ -0,0 +1,43 @@
+""""
+Cost function used to decide priority in priority queue for BFS
+
+Score calculated by counting the number of elements which are already
+sorted
+
+Nina
+
+2017-5-17
+"""
+def main():
+
+    #gene = [5,2,6,1,4,3,7,11,13,8,9,10]
+    gene = [1,2,3,5,4]
+    score = 0
+    priority = 0
+    repeat = 1
+
+    # calculate the maxscore
+    maxscore = sum(gene) - len(gene)
+    print(maxscore)
+
+
+    # calculate score
+    for i in range (0, len(gene)-1):
+
+        if gene[i] == gene[i + 1] + 1 or gene[i] == gene[i + 1] - 1:
+            score += repeat
+            repeat += 1
+
+        else: #gene[i] != gene[i+1]+1 or gene[i] != gene[i+1]-1:
+            score += 0
+            repeat = 1
+
+    print(score)
+
+    # converts score to priority
+    priority = maxscore / score
+    print(priority)
+    return(priority)
+
+if __name__ == "__main__":
+    main()
